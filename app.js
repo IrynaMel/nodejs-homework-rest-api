d559a35dc3188f440311cbbe9736452f1b3ccd83
IrynaMel <iryna.melnyk270692@gmail.com>
2022-06-25 12:48:10 +0300

    add mongo

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const contactsRouter = require("./routes/api/contacts");
+const DB_HOST =
+  "mongodb+srv://Iryna:Iryna2706@cluster0.hej5ddz.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
 const app = express();
 
@@ -14,12 +17,14 @@ app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 
-app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
-});
-
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
-});
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
 module.exports = app;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { validation } = require("../../validation/validation");
+const {
+  validation,
+  validationFavorite,
+} = require("../../validation/validation");
 const controllers = require("../../controllers/controllers");
 
 router.get("/", controllers.getContacts);
@@ -12,5 +15,11 @@ router.post("/", validation, controllers.postContact);
 router.delete("/:contactId", controllers.deleteContact);
 
 router.put("/:contactId", validation, controllers.putContact);
+
+router.patch(
+  "/:contactId/favorite",
+  validationFavorite,
+  controllers.updateStatus
+);
 
 module.exports = router;
